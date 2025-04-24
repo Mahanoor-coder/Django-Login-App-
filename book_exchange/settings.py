73b66670ccb3a849ff01080ef4f9d764ff634ac7
Mahanoor <mahanorali124@gmail.com>
2025-04-24 17:17:26 +0500

    Email
    Update setting.py file

--- book_exchange/settings.py
+++ book_exchange/settings.py
@@ -108,9 +108,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mahanorali124@gmail.com' 
-EMAIL_HOST_PASSWORD = 'njin canr xcmx xrpk'  
-DEFAULT_FROM_EMAIL = 'mahanorali124@gmail.com'  
+EMAIL_HOST_USER = ''# Write your email here 
+EMAIL_HOST_PASSWORD = ''  # Write your email password here
+DEFAULT_FROM_EMAIL = '' # Write your email here  
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
